5f190961480ebe7c9704784a84a2478486519e7a
EMoreno65 <ethanmichaelmoreno@gmail.com>
2026-05-27 08:17:26 -0600

    Moved the file to backend folder. We'll see if I can deploy it

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "prisma/config";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: require("path").resolve(__dirname, ".env") });
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
